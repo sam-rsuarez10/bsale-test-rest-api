3c77d586d425c20161931c99b7ffeba9f9447815
sam-rsuarez10 <suarezzcode2001@gmail.com>
2022-11-10 23:53:45 -0300

    Add __str__ function to each model

--- rest_api/models.py
+++ rest_api/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
     class Meta:
         verbose_name_plural = "categories"
 # end class
@@ -16,6 +19,9 @@ class Product(models.Model):
     price = models.FloatField()
     discount = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 #end class
 
 
